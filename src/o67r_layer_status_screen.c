9b76d32a48fcb9efa822021427e975e2769a3580
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-18 09:29:35 +0900

    codex test

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -26,6 +26,7 @@ static void set_initial_layer_text(lv_obj_t *label) {
 lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_t *screen = lv_obj_create(NULL);
     lv_obj_clear_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
 
@@ -33,6 +34,7 @@ lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_set_width(layer_label, 220);
     lv_obj_set_style_text_font(layer_label, &lv_font_montserrat_32, LV_PART_MAIN);
     lv_obj_set_style_text_align(layer_label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
+    lv_obj_set_style_text_color(layer_label, lv_color_hex(0xffffff), LV_PART_MAIN);
     set_initial_layer_text(layer_label);
     lv_obj_align(layer_label, LV_ALIGN_CENTER, 0, 0);
 
