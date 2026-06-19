4f0d03655951cbb35490004929350c5c1c61d762
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-19 22:20:32 +0900

    test RGB565

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -10,7 +10,7 @@
 
 #define SCREEN_SIZE 240
 
-extern const lv_img_dsc_t disp;
+extern const lv_image_dsc_t disp;
 
 lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_t *screen = lv_obj_create(NULL);
@@ -20,8 +20,8 @@ lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
-    lv_obj_t *image = lv_img_create(screen);
-    lv_img_set_src(image, &disp);
+    lv_obj_t *image = lv_image_create(screen);
+    lv_image_set_src(image, &disp);
     lv_obj_center(image);
 
     return screen;
