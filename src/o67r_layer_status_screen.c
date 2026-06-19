665c0a04ee20a6846a11d22bb392652770067936
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-19 22:06:01 +0900

    test RGB565

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -7,94 +7,22 @@
 #include <lvgl.h>
 
 #include <zmk/display/status_screen.h>
-#include <zmk/keymap.h>
-
-#define LAYER_LABEL_WIDTH 170
-#define LAYER_LABEL_HEIGHT 44
-//#define LAYER_LABEL_Y_OFFSET 28
-#define LAYER_LABEL_Y_OFFSET 8
-//#define LAYER_LABEL_Y_OFFSET 0
 
 #define SCREEN_SIZE 240
-#define BOTTOM_CLEAR_HEIGHT 56
 
-#define PATTERN_SIZE 212
-#define PATTERN_BORDER_WIDTH 42
-#define PATTERN_FILL_WIDTH 36
-#define PATTERN_BORDER_COLOR 0x9aa1a8
-#define PATTERN_FILL_COLOR 0x1457a7
-
-static void set_initial_layer_text(lv_obj_t *label) {
-    zmk_keymap_layer_index_t index = zmk_keymap_highest_layer_active();
-    const char *name = zmk_keymap_layer_name(zmk_keymap_layer_index_to_id(index));
-
-    if (name != NULL && name[0] != '\0') {
-        lv_label_set_text(label, name);
-    } else {
-        lv_label_set_text_fmt(label, "Layer %u", index);
-    }
-}
-
-static void style_pattern_arc(lv_obj_t *arc, uint32_t color, lv_coord_t width) {
-    lv_obj_remove_style_all(arc);
-    lv_obj_set_size(arc, PATTERN_SIZE, PATTERN_SIZE);
-    lv_obj_center(arc);
-    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
-    lv_obj_clear_flag(arc, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_arc_color(arc, lv_color_hex(color), LV_PART_INDICATOR);
-    lv_obj_set_style_arc_opa(arc, LV_OPA_COVER, LV_PART_INDICATOR);
-    lv_obj_set_style_arc_width(arc, width, LV_PART_INDICATOR);
-    lv_obj_set_style_arc_rounded(arc, true, LV_PART_INDICATOR);
-    lv_obj_set_style_arc_opa(arc, LV_OPA_TRANSP, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(arc, LV_OPA_TRANSP, LV_PART_KNOB);
-    lv_obj_set_style_border_opa(arc, LV_OPA_TRANSP, LV_PART_KNOB);
-}
-
-static void create_pattern_arc(lv_obj_t *screen, uint16_t start, uint16_t end) {
-    lv_obj_t *border = lv_arc_create(screen);
-    style_pattern_arc(border, PATTERN_BORDER_COLOR, PATTERN_BORDER_WIDTH);
-    lv_arc_set_angles(border, start, end);
-
-    lv_obj_t *fill = lv_arc_create(screen);
-    style_pattern_arc(fill, PATTERN_FILL_COLOR, PATTERN_FILL_WIDTH);
-    lv_arc_set_angles(fill, start, end);
-}
-
-static void create_background_pattern(lv_obj_t *screen) {
-    create_pattern_arc(screen, 22, 68);
-    create_pattern_arc(screen, 112, 158);
-    create_pattern_arc(screen, 202, 248);
-    create_pattern_arc(screen, 292, 338);
-}
+extern const lv_image_dsc_t disp;
 
 lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_t *screen = lv_obj_create(NULL);
+    lv_obj_remove_style_all(screen);
+    lv_obj_set_size(screen, SCREEN_SIZE, SCREEN_SIZE);
     lv_obj_clear_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
-    create_background_pattern(screen);
-
-    lv_obj_t *layer_label = lv_label_create(screen);
-    lv_obj_remove_style_all(layer_label);
-    lv_obj_set_size(layer_label, LAYER_LABEL_WIDTH, LAYER_LABEL_HEIGHT);
-    lv_label_set_long_mode(layer_label, LV_LABEL_LONG_CLIP);
-    lv_obj_set_style_bg_color(layer_label, lv_color_hex(0x000000), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(layer_label, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_set_style_text_font(layer_label, &lv_font_montserrat_32, LV_PART_MAIN);
-    lv_obj_set_style_text_align(layer_label, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
-    lv_obj_set_style_text_color(layer_label, lv_color_hex(0xffffff), LV_PART_MAIN);
-    set_initial_layer_text(layer_label);
-    lv_obj_align(layer_label, LV_ALIGN_CENTER, 0, LAYER_LABEL_Y_OFFSET);
-
-    lv_obj_t *bottom_clear = lv_obj_create(screen);
-    lv_obj_remove_style_all(bottom_clear);
-    lv_obj_set_size(bottom_clear, SCREEN_SIZE, BOTTOM_CLEAR_HEIGHT);
-    lv_obj_set_style_bg_color(bottom_clear, lv_color_hex(0x000000), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(bottom_clear, LV_OPA_COVER, LV_PART_MAIN);
-    lv_obj_clear_flag(bottom_clear, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_align(bottom_clear, LV_ALIGN_BOTTOM_MID, 0, 0);
-    lv_obj_move_foreground(bottom_clear);
+    lv_obj_t *image = lv_image_create(screen);
+    lv_image_set_src(image, &disp);
+    lv_obj_center(image);
 
     return screen;
 }
