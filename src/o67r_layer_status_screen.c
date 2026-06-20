52880a2a909ae165e41d810525c313a6a9c890b8
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-20 18:51:00 +0900

    add button label

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -160,7 +160,7 @@ static void create_separator(lv_obj_t *screen, lv_coord_t x, lv_coord_t y, lv_co
     lv_obj_remove_style_all(separator);
     lv_obj_set_pos(separator, x, y);
     lv_obj_set_size(separator, width, height);
-    lv_obj_set_style_bg_color(separator, lv_color_hex(0xb0b0b0), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(separator, lv_color_hex(0x808080), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(separator, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_clear_flag(separator, LV_OBJ_FLAG_SCROLLABLE);
 }
