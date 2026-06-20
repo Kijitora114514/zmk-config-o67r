ad7ddf0f4efacf9afe19e9000f80db4452f7cca1
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-20 15:50:12 +0900

    update keymap

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -26,14 +26,14 @@
 
 extern const lv_image_dsc_t disp;
 
-static lv_color_t rgb_to_gbr(const lv_color_filter_dsc_t *filter, lv_color_t color,
+static lv_color_t rgb_to_bgr(const lv_color_filter_dsc_t *filter, lv_color_t color,
                              lv_opa_t opacity) {
     LV_UNUSED(filter);
     LV_UNUSED(opacity);
 
     return (lv_color_t){
-        .red = color.green,
-        .green = color.blue,
+        .red = color.blue,
+        .green = color.green,
         .blue = color.red,
     };
 }
@@ -193,7 +193,7 @@ static void init_swipe_status(lv_obj_t *screen) {
 #endif
 
 lv_obj_t *zmk_display_status_screen(void) {
-    static lv_color_filter_dsc_t rgb_to_gbr_filter;
+    static lv_color_filter_dsc_t rgb_to_bgr_filter;
 
     lv_obj_t *screen = lv_obj_create(NULL);
     lv_obj_remove_style_all(screen);
@@ -203,8 +203,8 @@ lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
     lv_obj_t *image = lv_image_create(screen);
-    lv_color_filter_dsc_init(&rgb_to_gbr_filter, rgb_to_gbr);
-    lv_obj_set_style_color_filter_dsc(image, &rgb_to_gbr_filter, LV_PART_MAIN);
+    lv_color_filter_dsc_init(&rgb_to_bgr_filter, rgb_to_bgr);
+    lv_obj_set_style_color_filter_dsc(image, &rgb_to_bgr_filter, LV_PART_MAIN);
     lv_obj_set_style_color_filter_opa(image, LV_OPA_COVER, LV_PART_MAIN);
     lv_image_set_src(image, &disp);
     lv_obj_center(image);
