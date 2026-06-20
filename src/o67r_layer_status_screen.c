bc110a9ad94af395b667875c867ca2665b085d7a
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-20 10:12:42 +0900

    update GBR

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -12,7 +12,21 @@
 
 extern const lv_image_dsc_t disp;
 
+static lv_color_t rgb_to_gbr(const lv_color_filter_dsc_t *filter, lv_color_t color,
+                             lv_opa_t opacity) {
+    LV_UNUSED(filter);
+    LV_UNUSED(opacity);
+
+    return (lv_color_t){
+        .red = color.green,
+        .green = color.blue,
+        .blue = color.red,
+    };
+}
+
 lv_obj_t *zmk_display_status_screen(void) {
+    static lv_color_filter_dsc_t rgb_to_gbr_filter;
+
     lv_obj_t *screen = lv_obj_create(NULL);
     lv_obj_remove_style_all(screen);
     lv_obj_set_size(screen, SCREEN_SIZE, SCREEN_SIZE);
@@ -21,6 +35,10 @@ lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
 
     lv_obj_t *image = lv_image_create(screen);
+    lv_color_filter_dsc_init(&rgb_to_gbr_filter);
+    rgb_to_gbr_filter.filter_cb = rgb_to_gbr;
+    lv_obj_set_style_color_filter_dsc(image, &rgb_to_gbr_filter, LV_PART_MAIN);
+    lv_obj_set_style_color_filter_opa(image, LV_OPA_COVER, LV_PART_MAIN);
     lv_image_set_src(image, &disp);
     lv_obj_center(image);
 
