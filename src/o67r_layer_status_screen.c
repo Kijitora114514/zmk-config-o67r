018de41fd8e9fe0cc445f304d83573619b2bd191
Kijitora114514 <scas2@hotmail.co.jp>
2026-06-21 15:17:38 +0900

    delete boards overlay

--- src/o67r_layer_status_screen.c
+++ src/o67r_layer_status_screen.c
@@ -226,6 +226,9 @@ static void create_separator(lv_obj_t *screen, lv_coord_t x, lv_coord_t y, lv_co
     lv_obj_set_size(separator, width, height);
     lv_obj_set_style_bg_color(separator, lv_color_hex(DISPLAY_GRAY_HEX), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(separator, LV_OPA_COVER, LV_PART_MAIN);
+    lv_obj_set_style_outline_color(separator, lv_color_hex(0x000000), LV_PART_MAIN);
+    lv_obj_set_style_outline_width(separator, 1, LV_PART_MAIN);
+    lv_obj_set_style_outline_opa(separator, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_clear_flag(separator, LV_OBJ_FLAG_SCROLLABLE);
 }
 
